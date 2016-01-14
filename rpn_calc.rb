8a7987442e3222fd3cd1890f2e96451ad9a98c74
GeoffPurdy <gdpurdy@gmail.com>
2016-01-14 14:32:47 -0500

    added stub methods; 1st failing test

--- rpn_calc.rb
+++ rpn_calc.rb
@@ -2,6 +2,22 @@
 class RpnCalc
 
    def initialize
+      @stack = []
+   end
+
+   def read
+   end
+
+   def write
+   end
+
+   def isNumeric?(text)
+   end
+
+   def isOperator(text)
+   end
+
+   def isQuit?(text)
    end
 
 end
--- tc_rpn_calc.rb
+++ tc_rpn_calc.rb
@@ -1,10 +1,21 @@
-require_relative "rpn-calc"
+require_relative "rpn_calc"
 require "test/unit"
 
 class TestRpnCalc < Test::Unit::TestCase
+   #The calculator should use standard input and standard output, 
+   #unless the language makes that impossible.
 
-   def test_add
-      assert(true) #placeholder
+   #It should implement the four standard arithmetic operators.
+
+   #It should support negative and decimal numbers, 
+   #and should not have arbitrary limits on the number of operations.
+
+   #The calculator should not allow invalid or undefined behavior.
+
+   #The calculator should exit when it receives a q command 
+   #or an end of input indicator (EOF).
+   def test_quit
+      assert( RpnCalc.new().isQuit?('q'), "Failed to quit" )
+      assert( RpnCalc.new().isQuit?('0'), "Quit when should not")
    end
-
 end
