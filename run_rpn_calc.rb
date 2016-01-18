4a0873fbc1da285b162fea28e972e3b9f7802138
GeoffPurdy <gdpurdy@gmail.com>
2016-01-18 15:04:06 -0500

    cleaned up front-end code

--- run_rpn_calc.rb
+++ run_rpn_calc.rb
@@ -1,23 +1,25 @@
 require_relative 'rpn_calc.rb'
 
 PROMPT = "> "
-QUIT_SIGNAL = ['q', nil] # EOF is nil
+QUIT_SIGNAL = "q\n"
 ERROR_MSG = 'ERROR'
 
 calc = RpnCalc.new()
 
-print PROMPT 
-while !QUIT_SIGNAL.include?(input = gets.chomp)
+print PROMPT
+ 
+STDIN.each_line do |input_line|
+   exit if (input_line.nil? || input_line == QUIT_SIGNAL)
    begin
-      calc.enter(input)
+      calc.enter(input_line.chomp)
       puts calc.display
       print PROMPT
    rescue
+      # calculator is in an invalid state 
+      # and further operations would yield erroneous results
+      # example:  division by zero
       puts ERROR_MSG
       abort
    end
 end
-
-exit
-
 
